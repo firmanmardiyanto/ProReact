3b41f746dfc14f4e6f0ec3972da4ff00944cd0ce
Firman Mardiyanto <firman.mardiyanto@gmail.com>
2020-08-17 00:52:42 +0700

    web service json server crud

--- productapp/src/store/index.js
+++ productapp/src/store/index.js
@@ -4,17 +4,28 @@ import stateReducer from "./stateReducer";
 import { customReducerEnhancer } from "./customReducerEnhancer";
 import { multiActions } from "./multiActionMiddleware";
 import { asyncEnhancer } from "./asyncEnhancer";
-
+import { createRestMiddleware } from "../webservice/RestMiddleware";
 const enhancedReducer = customReducerEnhancer(
-    combineReducers(
-        {
-            modelData: modelReducer, 
-            stateData: stateReducer
-        })
+  combineReducers({
+    modelData: modelReducer,
+    stateData: stateReducer,
+  })
 );
-
-export default createStore(enhancedReducer, 
-    compose(applyMiddleware(multiActions), asyncEnhancer(2000)));
-
-export { saveProduct, saveSupplier, deleteProduct, deleteSupplier } 
-    from "./modelActionCreators";
+const restMiddleware = createRestMiddleware(
+  "http://localhost:3500/api/products",
+  "http://localhost:3500/api/suppliers"
+);
+export default createStore(
+  enhancedReducer,
+  compose(
+    applyMiddleware(multiActions),
+    applyMiddleware(restMiddleware),
+    asyncEnhancer(2000)
+  )
+);
+export {
+  saveProduct,
+  saveSupplier,
+  deleteProduct,
+  deleteSupplier,
+} from "./modelActionCreators";
